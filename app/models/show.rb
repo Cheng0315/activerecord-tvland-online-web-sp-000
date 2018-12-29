81846455bfb919bd6f76b23a7fd3c79a1c5fde74
Cheng Vue <cvu3@ucsc.edu>
2018-12-29 20:41:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :networks
+
+  def build_network(letters)
+    Network.new(call_letters: letters)
+  end
+
+  def network
+
+  end
+
 end
